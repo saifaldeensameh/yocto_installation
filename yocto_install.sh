23aa3c564bf9b437ceb0caf4b07403bb1083d401
saifaldeensameh <seifeldeens15@gmail.com>
2024-04-01 23:11:25 +0200

    commenting stm32mp1 package

--- yocto_install.sh
+++ yocto_install.sh
@@ -13,13 +13,13 @@ mkdir yocto
 cd yocto
 
 git clone https://github.com/yoctoproject/poky.git
-git clone https://github.com/STMicroelectronics/meta-st-stm32mp.git
+#git clone https://github.com/STMicroelectronics/meta-st-stm32mp.git
 git clone https://github.com/openembedded/meta-openembedded.git
 
 cd poky/
 git checkout kirkstone
-cd ../meta-st-stm32mp/
-git checkout kirkstone
+#cd ../meta-st-stm32mp/
+#git checkout kirkstone
 cd ../meta-openembedded/
 git checkout kirkstone
 
